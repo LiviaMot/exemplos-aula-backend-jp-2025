1df3aea92ecb83c663a2eb5cc18c4f8b3ee8e5b0
Livia <liviamtavares05@gmail.com>
2025-09-17 21:41:22 -0300

    WIP: exercicios

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
 import express from "express"
+import { exercicio1, exercicio2 } from "./services/exercicio.js"
 
 const app = express()
-
-app.get('/exercicio1', (req, res) => {
-    const num1 = req.query.num1
-    const num2 = req.query.num2
-    console.log(req.params)
     
-    res.status(200).send("Hello "+ nome)
-})
-app.get('/exercicio1/:num1/:num2', (req, res) => {
-    const num1 = req.params.num1
-    const num2 = req.params.num2
-    console.log(req.params)
-    
-    res.status(200).send("Resultado "+ num1+num2)
-})
+// localhost:3000/exercicio1?num1=30&num2=45
+app.get('/exercicio1', exercicio1)
 
-app.get('/api/pessoa/:id', (req, res) => {
-    const nome = req.query.nome
-    console.log(req.params)
-    
-    res.status(200).send("Hello "+ nome)
-})
-
-app.get('/api/pessoa/:id/teste/:outroId', (req, res) => {
-    const nome = req.query.nome
-    console.log(req.params)
-    
-    res.status(200).send("Hello "+ nome)
-})
-
-app.get('/api/pessoa/:id', (req, res) => {
-    const nome = req.query.nome
-    console.log(req.params)
-    
-    res.status(200).send("Hello "+ nome)
-})
-
+// localhost:3000/exercicio2/10/2
+app.get('/exercicio2/:valorHoras/:qtdeHoras', exercicio2)
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")

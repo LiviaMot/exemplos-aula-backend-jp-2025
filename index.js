07f67d6ad14be6a452f05a514431363288c6c735
Livia <liviamtavares05@gmail.com>
2025-09-24 20:52:49 -0300

    feat: exercicios finalizados

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { exercicio1, exercicio2 } from "./services/exercicio.js"
+import { exercicio1, exercicio2, exercicio3, exercicio4, exercicio5 } from "./services/exercicio.js"
 
 const app = express()
     
@@ -8,6 +8,15 @@ app.get('/exercicio1', exercicio1)
 
 // localhost:3000/exercicio2/10/2
 app.get('/exercicio2/:valorHoras/:qtdeHoras', exercicio2)
+
+// localhost:3000/exercicio3/5/5/5/5/5
+app.get('/exercicio3/:peso/:peso2/:peso3/:peso4/:peso5', exercicio3)
+
+// localhost:3000/exercicio4/50
+app.get('/exercicio4/:celsius', exercicio4)
+
+// localhost:3000/exercicio5/50
+app.get('/exercicio5/:mile', exercicio5)
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
--- services/exercicio.js
+++ services/exercicio.js
@@ -4,10 +4,10 @@ export const exercicio1 = (req, res) => {
   const result = num1 + num2
 
   res.status(200).send({
-      num1,
-      num2,
-      result
-      // result: num1 + num2
+    num1,
+    num2,
+    result
+    // result: num1 + num2
   })
 }
 
@@ -17,6 +17,37 @@ export const exercicio2 = (req, res) => {
   const { valorHoras, qtdeHoras } = req.params
 
   res.status(200).send({
-      result: valorHoras * qtdeHoras
+    result: valorHoras * qtdeHoras
+  })
+}
+
+export const exercicio3 = (req, res) => {
+  const { peso, peso2, peso3, peso4, peso5 } = req.params
+  
+  // +peso = operador unário; converte as strings em números (colocadas a frente do nome da variavel)
+  const soma = +peso + +peso2 + +peso3 + +peso4 + +peso5
+  const media = soma / 5
+
+  res.status(200).send({
+    result: media
+  })
+}
+
+export const exercicio4 = (req, res) => {
+  const { celsius } = req.params
+
+  const fahre = (9 * +celsius + 160) / 5
+
+  res.status(200).send({
+    result: fahre
+  })
+}
+
+export const exercicio5 = (req, res) => {
+  const { mile } = req.params
+  const km = +mile * 1.609
+
+  res.status(200).send({
+    result: km
   })
 }

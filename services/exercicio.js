f1e2b2322a112fda5b8e58ea17e2771a03f3cc9b
Livia <liviamtavares05@gmail.com>
2025-09-29 15:46:55 -0300

    feat: exercicios 1 a 5 em get e post

--- services/exercicio.js
+++ services/exercicio.js
@@ -1,38 +1,20 @@
+// +peso = operador unário; converte as strings em números (colocadas a frente do nome da variavel)
 export function somar(num1, num2) {
-  return num1 + num2;
+  return +num1 + +num2;
 }
 
 export function salario(valorHora, qtdeHora) {
-  return valorHora * qtdeHora;
+  return +valorHora * +qtdeHora;
 }
 
-export const exercicio3 = (req, res) => {
-  const { peso, peso2, peso3, peso4, peso5 } = req.params
-  
-  // +peso = operador unário; converte as strings em números (colocadas a frente do nome da variavel)
-  const soma = +peso + +peso2 + +peso3 + +peso4 + +peso5
-  const media = soma / 5
-
-  res.status(200).send({
-    result: media
-  })
+export function media(peso, peso2, peso3, peso4, peso5) {
+  return (+peso + +peso2 + +peso3 + +peso4 + +peso5) / 5
 }
 
-export const exercicio4 = (req, res) => {
-  const { celsius } = req.params
-
-  const fahre = (9 * +celsius + 160) / 5
-
-  res.status(200).send({
-    result: fahre
-  })
+export function celsiusToFah(celsius) {
+  return (9 * +celsius + 160) / 5
 }
 
-export const exercicio5 = (req, res) => {
-  const { mile } = req.params
-  const km = +mile * 1.609
-
-  res.status(200).send({
-    result: km
-  })
+export function milhaToKm(mile) {
+  return +mile * 1.609
 }
